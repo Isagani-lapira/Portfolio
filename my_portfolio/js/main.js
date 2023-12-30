4ff7e9c7e536f36cc2dc975d80d327154a6e3f6a
Isagani Lapira <lapiraisagani@gmail.com>
2023-12-30 16:12:03 +0800

    Add: about page

--- my_portfolio/js/main.js
+++ my_portfolio/js/main.js
@@ -2,17 +2,9 @@ $(document).ready(function(){
 
     $('ul a').on('click', function(e) {
         var id = $(this).attr('href'); 
-        
-        // hide everything
-        $('main .tabs').each(function(){
-            $(this).addClass('hidden')
-        })
 
          // remove 'bg-secondary' from all spans
          $('ul span').removeClass('bg-secondary');
-         // open the selected tab
-         $(id).removeClass('hidden');
-         console.log(id)
          $(this).find('span').addClass('bg-secondary'); //highlight selected tab
     });
 })
